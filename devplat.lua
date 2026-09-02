30f2ad5ac6dfe88a110607ea71f4e9726a0fd982
TheFortniteFreak <126966852+TheFortniteFreak@users.noreply.github.com>
2026-09-02 16:47:08 +0200

    Create devplat.lua

---
+++ devplat.lua
@@ -0,0 +1,79 @@
+return {
+            ["Windows"] = {
+                ["A"] = false,
+                ["B"] = false,
+                ["C"] = true,
+                ["D"] = "0.736.0.7361348",
+                ["E"] = false,
+                ["F"] = false,
+                ["G"] = true,
+                ["H"] = true,
+                ["I"] = false
+            },
+            ["Linux"] = {
+                ["A"] = false,
+                ["B"] = false,
+                ["C"] = false,
+                ["D"] = "0.736.0.7361348",
+                ["E"] = false,
+                ["F"] = false,
+                ["G"] = true,
+                ["H"] = true,
+                ["I"] = false
+            },
+            ["MacOS"] = {
+                ["A"] = false,
+                ["B"] = false,
+                ["C"] = false,
+                ["D"] = "0.736.0.7361348",
+                ["E"] = false,
+                ["F"] = false,
+                ["G"] = true,
+                ["H"] = true,
+                ["I"] = true
+            },
+            ["Android"] = {
+                ["A"] = false,
+                ["B"] = false,
+                ["C"] = false,
+                ["D"] = "0.736.0.7361348",
+                ["E"] = true,
+                ["F"] = true,
+                ["G"] = false,
+                ["H"] = false,
+                ["I"] = false
+            },
+            ["VR"] = {
+                ["A"] = true,
+                ["B"] = false,
+                ["C"] = true,
+                ["D"] = "0.736.0.7361348",
+                ["E"] = false,
+                ["F"] = false,
+                ["G"] = true,
+                ["H"] = true,
+                ["I"] = false
+            },
+            ["Console"] = {
+                ["A"] = false,
+                ["B"] = true,
+                ["C"] = false,
+                ["D"] = "0.736.0.7361348",
+                ["E"] = false,
+                ["F"] = false,
+                ["G"] = true,
+                ["H"] = true,
+                ["I"] = false
+            },
+            ["IOS"] = {
+                ["A"] = false,
+                ["B"] = false,
+                ["C"] = false,
+                ["D"] = "0.736.0.7361348",
+                ["E"] = false,
+                ["F"] = true,
+                ["G"] = false,
+                ["H"] = false,
+                ["I"] = true
+            }
+        }
